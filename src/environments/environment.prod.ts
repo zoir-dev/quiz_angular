909744b13571ff4dedc4293cfaa1c67d0a9227a4
Zoirjon <zoirjonabduvohidov0@gmail.com>
2024-02-27 17:58:14 +0500

    api changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseApi: 'http://167.71.49.153:5300/api',
+  baseApi: 'http://161.97.102.90:5300/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseApi: 'http://167.71.49.153:5300/api',
+  baseApi: 'http://161.97.102.90:5300/api',
 };
 
 /*
